3f9ad61e9778d3e79a3c8d6dd306941d48c954b3
bdb <bhuiya@github.com>
2018-06-12 23:16:35 -0400

    Excercise 3

--- Ex3.js
+++ Ex3.js
@@ -1,43 +1,52 @@
-let amount=4;
-let left =0;
-let temp=0;
-let denominations=[1,2,3];
-for(let i=0; i<amount; i++){
-  
-  if(i==0)
-  {
-   let temp= denominations[i];
-   console.log(temp);
-   if(amount%temp==0)
-   {
-     console.log(amount +' times 1 cent');
-   }
 
-  }
-  if(i==1)
-  {
-    left = (amount - denominations[i]);
-     temp= left/denominations[i-1]
-     console.log(temp +' times '+denominations[i-1]+ 'c  ' + '1 times '+denominations[i]+ 'c');
-     //  console.log(left);
-    
-  }
-  
-  if(i==2)
-  {
-    left = (amount - denominations[i]);
-     temp= left/denominations[i-2]
-     console.log(temp +' times '+denominations[i-2]+ 'c  ' + '1 times '+denominations[i]+ 'c');
-     //  console.log(left);
-    
-  }
+#!/usr/bin/env node
 
-  if(i==3)
-  {
-    left = (amount - denominations[i-2]);
-     temp= left/denominations[i-2]
-     console.log('2 times '+denominations[i-2]+ 'c  ' );
-    
-    
-  }
+function findPermutations(coins, n) {
+
+    var buffer = new Array(n + 1);
+    for (var i = 0; i <= n; ++i)
+        buffer[i] = new Array(coins.length + 1);
+
+ 
+    for (var j = 1; j <= coins.length; ++j)
+        buffer[0][j] = 1;
+
+      for (i = 1; i <= n; ++i) {
+             buffer[i][0] = 0;
+
+               for (j = 1; j <= coins.length; ++j) {
+
+         
+            var value = buffer[i][j - 1];
+
+            if (coins[j - 1] <= i)
+                value += buffer[i - coins[j - 1]][j];
+
+         
+            buffer[i][j] = value;
+        }
+    }
+
+
+    return buffer[n][coins.length];
 }
+
+
+function processData(input) {
+    var lines = input.split('\n');
+    var coins = lines[0].split(',').map(function (s) {return +s;});
+    var n = +lines[1];
+    var res = findPermutations(coins, n);
+    console.log(res);
+}
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
